27f185959333d2a7e4a8c6f45c7db27bcf01c152
Lucas Queiroz <l_queiroz@outlook.com.br>
2021-01-25 22:47:13 -0300

    Animação do Scroll com Javascript

--- script.js
+++ script.js
@@ -67,3 +67,27 @@ function initScrollSmooth() {
   });
 }
 initScrollSmooth();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.7;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
